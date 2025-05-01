afd0be44f773ef7ebd7455ce245f66a06bebddb4
iam-navyaK <keerthanakoya10@gmail.com>
2025-05-01 16:23:07 +0530

    upated server js file

--- visitor-counter/server.js
+++ visitor-counter/server.js
@@ -4,16 +4,18 @@ const cors = require('cors');
 const redis = require('redis');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017/visitorsDB', {
- 
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/visitorsDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
 });
@@ -26,7 +28,10 @@ const visitSchema = new mongoose.Schema({
 const Visit = mongoose.model('Visit', visitSchema);
 
 // Redis connection
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
+
 redisClient.on('error', (err) => console.error('Redis error:', err));
 
 (async () => {
